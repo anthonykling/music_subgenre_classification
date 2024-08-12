530345fa5fcaa28c0db73800436a1b8e274109fe
anthonykling <klinganthonym@gmail.com>
2024-08-12 16:59:50 -0400

    functions used to generate the full dataframe

--- dataset_build_helpers.py
+++ dataset_build_helpers.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import json
 import os
+import sys
+from queue import LifoQueue
+import numpy as np
+from warnings import simplefilter
+simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
 def get_folder_size(folder_path):
     '''
@@ -49,3 +54,118 @@ def json_to_df_list(folder_path):
     print('List of DataFrames has been made!\nConcatenate them next!')
 
     return dfs
+
+def json_to_dictionary(filename) :
+  '''
+  Loads a json file into a dictionary.
+  '''
+  if (filename[-4:] != 'json') :
+    return
+  with open(filename, "r") as json_data :
+    data = json.load(json_data)
+    json_data.close()
+  return data
+
+def unpack_keys_single_level(raw_data, sep='_') -> pd.DataFrame:
+  '''
+     Unpacks a dictionary or list into a dataframe where each column represents a value or entry,
+     or returns a dataframe with a single entry for any other type.  
+  '''
+  if type(raw_data) == dict :
+    return pd.DataFrame(raw_data)
+  elif type(raw_data) == list :
+    length = len(raw_data)
+    return pd.DataFrame(raw_data, columns=list(range(0,length)))
+  else :
+    return pd.DataFrame(raw_data, columns=[sep])
+  
+def dictionary_to_dataframe(raw_dict_data, include_meta=True, sep='_') :
+  '''
+  Performs a depth first search of the dictionary, to create a pandas dataframe where each column
+  is a single bool, string, or float.  Column names are the nested key names concatenated and separated by
+  the sep argument.
+  '''
+  Q = LifoQueue()
+  names = LifoQueue()
+  cols = []
+  raw_data = []
+
+  if 'mbdata' in raw_dict_data:
+    mbdata_df = pd.json_normalize(raw_dict_data.pop('mbdata'))
+    mbdata_df = mbdata_df.rename(columns=lambda x: 'mbdata' + sep + x)
+
+  for k in raw_dict_data.keys():
+
+    if (k != 'metadata' or include_meta) :
+      Q.put(raw_dict_data[k])
+      names.put(k)
+
+
+  while(not Q.empty()) :
+    r = Q.get()
+    name = names.get()
+    if (type(r) == dict) :
+      for k1 in r.keys() :
+        if (k1 == "beats_position"):
+          continue
+        Q.put(r[k1])
+        names.put(name+sep+k1)
+    elif (type(r) == list) :
+      for i in range(0, len(r)) :
+        Q.put(r[i])
+        names.put(name+sep+str(i))
+    else :
+      cols.append(name)
+      raw_data.append(r)
+
+  df = pd.DataFrame([raw_data], columns=cols)
+
+  df = pd.concat([df.reset_index(drop=True), mbdata_df.reset_index(drop=True)], axis=1)
+
+  return df
+  
+def read_folder_to_dataframe(dir, sep) :
+  '''
+  Reads all json files in a directory dir into a single pandas dataframe.
+  '''
+  list_df = []
+  list_dir = os.listdir(dir)
+  num_dirs = len(list_dir)
+  one_perc = num_dirs//100
+  completed = 0
+
+  for subdir in list_dir :
+    if(subdir[-4:] != 'json') :
+      continue
+    d = json_to_dictionary(dir+subdir)
+    df = dictionary_to_dataframe(d, sep=sep)
+    list_df.append(df)
+    completed += 1
+    print("\r{}".format(completed).rjust(5) + " / " + str(num_dirs), end="")
+    sys.stdout.flush()
+
+  while len(list_df) > 1:
+    new_list = []
+
+    for i in range(0, len(list_df) - 1, 2):
+        combined_df = pd.concat([list_df[i], list_df[i + 1]], ignore_index=True)
+        new_list.append(combined_df)
+
+    if len(list_df) % 2 == 1:
+        new_list.append(list_df[-1])
+
+    list_df = new_list
+
+  return list_df[0]
+
+
+
+    
+
+  #while (len(list_df) > 1) :
+  #  new_list = [pd.concat(list_df[i:i+2]) for i in range(0, len(list_df)//2)]
+  #  if (len(list_df) % 2 == 1) :
+  #    new_list.append(list_df[-1])
+  #  list_df = new_list
+
+  return pd.concat(list_df)
